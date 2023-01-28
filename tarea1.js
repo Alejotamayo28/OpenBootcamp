7d84ce58fb70fd253454698825811ea168ec5468
Alejandro Vergara Tamayo <73759053+Alejotamayo28@users.noreply.github.com>
2023-01-28 12:01:52 -0500

    Add files via upload

---
+++ tarea1.js
@@ -0,0 +1,13 @@
+const persona ={
+    nombre: "Alejandro",
+    edad: 18,
+    Soy_desarrollador: false,
+    nacimiento: Date('2005-01-28'),
+    libro_favorito:{
+        titulo: "1984",
+        autor: "George Orwell",
+        fecha: new Date(1949-06-8),
+        url :"https://es.wikipedia.org/wiki/1984_(novela)"
+    }
+}
+console.log(persona.libro_favorito.fecha)
